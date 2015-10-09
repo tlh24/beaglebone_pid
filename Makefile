645b174e0e0d71251d81bbcc83ebcdce97b9f527
Demo User <debian@beaglebone.localdomain>
2015-10-09 00:33:32 +0000

    fiddling with configure script.  does not work yet.

--- Makefile
+++ Makefile
@@ -5,10 +5,17 @@ OBJS = bbb-eqep.o biphasic.o
 all: pid_tst
 
 %.o:%.cpp
-	g++ -std=c++11 -c -O2 -o $@ $<
+	g++-4.7 -std=c++11 -c -O2 -o $@ $<
 	
 pid_tst: $(OBJS)
-	g++ -o pid_tst $(OBJS)
+	g++-4.7 -o pid_tst $(OBJS)
+
+BB-pid0-00A0.dtbo: BB-pid0-00A0.dts
+	dtc -O dtb -b 0 $< -o $@
+
+install: BB-pid0-00A0.dtbo
+	sudo cp $< /lib/firmware
+	echo "echo BB-pid0 > /sys/devices/platform/bone_capemgr/slots"
 	
 clean:
-	rm -rf *.o pid_tst
+	rm -rf *.o pid_tst *.dtbo
--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
-echo cape-universaln > /sys/devices/bone_capemgr.*/slots
+# must be run as root!
+echo cape-universaln > /sys/devices/platform/bone_capemgr/slots
+# echo PyBBBIO-eqep0 > /sys/devices/platform/bone_capemgr/slots
 # echo bone_eqep0 > /sys/devices/bone_capemgr.9/slots
 # this will also pinmux P9.22 to pwm0A function.
 
@@ -13,10 +14,14 @@ config-pin P9.22 pwm
 config-pin P9.15 out
 config-pin P9.16 out
 
-echo 0 > /sys/class/pwm/export
-echo 50000 > /sys/class/pwm/pwm0/period_ns
-echo 0 > /sys/class/pwm/pwm0/duty_ns
-echo 1 > /sys/class/pwm/pwm0/run
+echo 0 > /sys/class/pwm/pwmchip0/export
+# echo 0 > /sys/class/pwm/export # kernel 3.8
+echo 50000 > /sys/devices/platform/ocp/48300000.epwmss/48300200.ehrpwm/pwm/pwmchip0/pwm0/period
+# echo 50000 > /sys/class/pwm/pwmchip0/period_ns
+echo 500 > /sys/devices/platform/ocp/48300000.epwmss/48300200.ehrpwm/pwm/pwmchip0/pwm0/duty_cycle
+echo 1 > /sys/devices/platform/ocp/48300000.epwmss/48300200.ehrpwm/pwm/pwmchip0/pwm0/enable
+# echo 0 > /sys/class/pwm/pwmchip0/duty_ns
+# echo 1 > /sys/class/pwm/pwmchip0/run
 #20kHz. enabled, but no pulses.
 
 #export gpio for P9.15 and P9.16 (forward and reverse)
@@ -28,3 +33,4 @@ echo out > /sys/class/gpio/gpio51/direction
 
 #turn on timer1. (timer0 is the 32kHz RTC)
 echo on > /sys/devices/ocp.3/48044000.timer/power/control
+# this is used for high-speed internal interval timing. 
