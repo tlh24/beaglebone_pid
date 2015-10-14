b9a3a2c71a9a65d6a7a0a384a0b3a4069beaa326
Tim Hanson <sideskate@gmail.com>
2015-10-14 11:12:33 -0700

    final try on the 4.1.x kernel...

--- biphasic.cpp
+++ biphasic.cpp
@@ -164,7 +164,7 @@ int main (int argc, char const *argv[])
 	control_addr[0x958 / 4] = 0xf; //P9.18 mode 7 (gpio0) pinmux, pull up/down disabled
 	control_addr[0x950 / 4] = 0xf; //***P9.22 mode 3 (PWM) pulldown off, fast slew, rx inactive. 
 	control_addr[0x84c / 4] = 0xf; //P9.16 mode 7 (gpio1) pulldown off, fast slew, rx inactive. 
-	printf("pin P9.17 0x%X\n", control_addr[0x95c / 4]);
+	printf("pin P9.17 0x%X\n", control_addr[0x95c / 4]); 
 	printf("pin P9.22 0x%X\n", control_addr[0x950 / 4]);
 	printf("pin P9.16 0x%X\n", control_addr[0x84c / 4]);
 	printf("pwmss_ctrl 0x%X\n", control_addr[0x664 / 4]);
