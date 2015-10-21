40b166f202ebe6beb1cadf2c976f29ca80ee72e7
Tim Hanson <sideskate@gmail.com>
2015-10-21 16:24:49 -0700

    shorter slowdown cycle

--- biphasic.cpp
+++ biphasic.cpp
@@ -365,7 +365,7 @@ int main (int argc, char const *argv[])
 			update_velocity(n, 0.07);
 			if(t < 0.0075){
 				dr = 1.0; //compress the spring down; stop just before it maxes out
-			}else if(t < 0.015 && x > 450){
+			}else if(t < 0.015 && x > 300){
 				if(x > cylbot - cyltop) dr = -1.0; //drive up.  near peak velocity @ crossing (when the slug will hit the actuator rod anyway)
 				else dr = -0.1; //coast up
 			}else if(t < 0.03){
