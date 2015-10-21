fb2aacdaa062e225e856bea989467b1c42127d8a
Tim Hanson <sideskate@gmail.com>
2015-10-21 12:27:24 -0700

    slower damping

--- biphasic.cpp
+++ biphasic.cpp
@@ -366,12 +366,12 @@ int main (int argc, char const *argv[])
 			update_velocity(n, 0.1);
 			if(t < 0.0075){
 				dr = 1.0; //compress the spring down; stop just before it maxes out
-			}else if(t < 0.016 || x > -800){
+			}else if(t < 0.016 || x > -700){
 				if(x > 0) dr = -1.0; //drive up.  near peak velocity @ crossing (when the slug will hit the actuator rod anyway)
 				else dr = -0.1; //coast up
 			}else if(t < 0.030){
 				if(v < -100*200){
-					dr = -0.10 * v / (600.0*200.0); 
+					dr = -0.08 * v / (600.0*200.0); 
 				}else{
 					dr = 0.0; 
 				}
