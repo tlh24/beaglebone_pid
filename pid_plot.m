a735afb44434e2df21af83ceaa294da8805ef026
Tim Hanson <sideskate@gmail.com>
2015-10-21 18:58:21 -0700

    pretty happy with performance now; need to integrate with the rest of the automation.

---
+++ pid_plot.m
@@ -0,0 +1,25 @@
+load pid.dat
+t = pid(:,1); 
+x = pid(:,2); 
+v = pid(:,3); 
+v_ = pid(:,4); 
+dr = pid(:,5); 
+
+%need to break into segments.  
+dt = diff(t); 
+dt = [dt; 0]; 
+ends = find(dt < -0.05); 
+plot(0,0); 
+hold on;
+s = 1; 
+for i = 1:length(ends)
+    e = ends(i); 
+% 	 figure
+    plot(t(s:e-1), dt(s:e-1)*24e6, 'm'); 
+% 	 hold on
+    plot(t(s:e), v_(s:e)/200, 'g'); 
+    plot(t(s:e), v(s:e)/200, 'b'); 
+    plot(t(s:e), x(s:e), 'k'); 
+    plot(t(s:e), dr(s:e)*1000, 'r'); 
+    s = e+1; 
+end
