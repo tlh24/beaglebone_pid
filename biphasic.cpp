9fa3d6f53e29d80307c20d507a73f5812ac5bf5d
Tim Hanson <sideskate@gmail.com>
2015-10-21 18:59:55 -0700

    pretty happy with performance now; need to integrate with the rest of the automation.

--- biphasic.cpp
+++ biphasic.cpp
@@ -369,7 +369,7 @@ int main (int argc, char const *argv[])
 			update_velocity(n, 0.2);
 			if(t < 0.0075){
 				dr = 1.0; //compress the spring down; stop just before it maxes out
-			}else if(t < 0.0168 && x > 400 + (j/3)*100){
+			}else if(t < 0.0168 && x > 400 ){ //+ (j/3)*100
 				if(x > cylbot - cyltop) dr = -1.0; //drive up.  near peak velocity @ crossing (when the slug will hit the actuator rod anyway)
 				else dr = -0.1; //coast up
 			}else if(t < 0.035){
