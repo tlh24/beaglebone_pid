8ddf6576152ce354755c6b01d6a5a71f951a3421
Tim Hanson <sideskate@gmail.com>
2015-10-22 21:36:57 -0700

    everything looks fine .. need to test with a needle loaded + the shuttle, which will change trajectory slightly.'

--- pid_plot.m
+++ pid_plot.m
@@ -21,9 +21,9 @@ if separate == 0
 	hold on;
 end
 s = 1; 
-for i = 1:length(ends)
-    e = ends(i); 
-	 if separate == 1
+if separate == 1
+	for i = 1:length(ends)
+		 e = ends(i); 
 		 fig = figure;
 		 set (fig, 'Units', 'normalized', 'Position', [0,0,1,1]);
 		 plot(t(s:e-1), dt(s:e-1)*24e6, 'm'); 
@@ -32,11 +32,29 @@ for i = 1:length(ends)
 		 plot(t(s:e), v(s:e)/200, 'b'); 
 		 plot(t(s:e), dr(s:e)*1000, 'r'); 
 		  plot(t(s:e), x(s:e), 'k'); 
-	 else
-		 plot(t(s:e), v(s:e)/200, 'b'); 
-		 plot(t(s:e), dr(s:e)*1000, 'r'); 
+	end
+else
+	for i = 1:length(ends)
+		 e = ends(i); 
+		 plot(t(s:e), v(s:e)/200, 'b');
+		 s = e+1; 
+	end
+	for i = 1:length(ends)
+		 e = ends(i); 
+		 plot(t(s:e), dr(s:e)*1000, 'r');
+		 s = e+1; 
+	end
+	for i = 1:length(ends)
+		 e = ends(i); 
 		 plot(t(s:e), x(s:e), 'k'); 
-	 end
-	 axis([0, 0.1, -1200, 1800])
-    s = e+1; 
+		 s = e+1; 
+	end
+	axis([0, 0.08, -1200, 2200])
+	q(1) = text(0.04, 1500, 'black = motor / slug position (encoder counts)');
+	q(2) = text(0.04, 1400, 'red = motor drive (1000 = DC)');
+	q(3) = text(0.04, 1300, 'blue = estimated velocity, arb units');
+	q(4) = text(0.04, 1200, 'simlulated insertions spaced at 300\mum');
+	for k = 1:4 
+		q(k).FontSize = 14; 
+	end
 end
