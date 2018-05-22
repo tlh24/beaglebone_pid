2f9157bf901e0d8a22046494d31ef31141322bad
Tim Hanson <tim@neuralink.com>
2018-05-22 16:58:31 -0700

    higher friction term.

--- biphasic.cpp
+++ biphasic.cpp
@@ -406,8 +406,8 @@ int main (int argc, char const *argv[])
 						if(cmd[0] == 'U') scl = -1.0; 
 						int sta = eqep.getPosition(); 
 						dr = scl * 1.5 * friction; 
-						while(dr*scl > friction * 0.5){
-							dr -= 0.005*scl; 
+						while(dr*scl > friction){
+							dr -= 0.0025*scl; 
 							motor_setDrive(dr); 
 							usleep(50000); 
 						}
